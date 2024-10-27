33738522f35fff8a680048eac7ed418ffcc11949
Samy <n4913819@gmail.com>
2024-10-26 20:46:56 -0400

    changes made from theia lab environment

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,24 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 4
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'name', 'description']
+
+admin.site.register(CarModel)
 
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['type', 'date']
+    inlines = [CarModelInline]
+    
+admin.site.register(CarMake)
 # Register models here
