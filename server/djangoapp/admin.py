d835a2d86adcb8708b8017d38fa14a72c71601a3
Samy <n4913819@gmail.com>
2024-11-12 17:21:23 -0500

    change made from the theia lab

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -2,22 +2,24 @@ from django.contrib import admin
 from .models import CarMake, CarModel
 
 # CarModelInline class
-class CarModelInline(admin.StackedInline):
+class CarModelInline(admin.TabularInline):
     model = CarModel
-    extra = 4
-
-# CarModelAdmin class
-class CarModelAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'name', 'description']
-
-# Register CarModel with CarModelAdmin
-admin.site.register(CarModel, CarModelAdmin)
+    extra = 3
 
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
-    fields = ['type', 'date']
+    list_display = ('name', 'description')
+    search_fields = ['name']
     inlines = [CarModelInline]
 
-# Register CarMake with CarMakeAdmin
+# CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    list_filter = ['car_make', 'type', 'year']
+    search_fields = ['name', 'car_make__name']
+
+# Registering models with their respective admins
 admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
+
 
